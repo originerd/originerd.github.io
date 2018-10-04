10c9cbcee420a6eba276fb2388286daef24a6bf8
Jitae Kim <originerd@gmail.com>
2018-10-05 00:16:36 +0900

    Remove unnecessary code and file

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,10 @@
 import { graphql, StaticQuery } from 'gatsby'
+import 'normalize.css'
 import PropTypes from 'prop-types'
 import React from 'react'
 import Helmet from 'react-helmet'
 
 import Header from './Header'
-import './layout.css'
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -19,26 +19,11 @@ const Layout = ({ children }) => (
     `}
     render={data => (
       <>
-        <Helmet
-          title={data.site.siteMetadata.title}
-          meta={[
-            { name: 'description', content: 'Sample' },
-            { name: 'keywords', content: 'sample, something' },
-          ]}
-        >
+        <Helmet title={data.site.siteMetadata.title}>
           <html lang="en" />
         </Helmet>
         <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: '0 auto',
-            maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0,
-          }}
-        >
-          {children}
-        </div>
+        {children}
       </>
     )}
   />
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,10 @@
-import { Link } from 'gatsby'
 import React from 'react'
 
 import Layout from '../components/Layout'
 
 const IndexPage = () => (
   <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1>Originerd</h1>
   </Layout>
 )
 
