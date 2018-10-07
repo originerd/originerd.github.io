e8b0b1f64a32620aecc2bb6dd7a58e0ad3af4a74
Jitae Kim <originerd@gmail.com>
2018-10-07 23:57:16 +0900

    Add navigation links on Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,41 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
-const Header = ({ siteTitle }) => (
-  <div>
-    <h1>
-      <Link to="/">{siteTitle}</Link>
-    </h1>
-  </div>
-)
+import '../styles/header.css'
+
+const navItems = ['about', 'posts', 'projects'];
+const navColors = ['yellow', 'pink'];
+
+const renderNavLinks = () => {
+  const { pathname } = window.location;
+
+  return navItems.filter((navItem) => pathname !== `/${navItem}/`)
+    .map((navItem, i) => (
+      <Link className={`nav-link nav-link--${navColors[i]}`} to={`/${navItem}/`}>
+        <h2 className="nav-link__content">
+          {navItem}
+        </h2>
+      </Link>
+    ));
+}
+
+const Header = ({ siteTitle }) => {
+  if (window.location.pathname === '/') {
+    return null;
+  }
+
+  return (
+    <nav className="header">
+      <Link className="nav-link" to="/">
+        <h1 className="nav-link__content">
+          {siteTitle}
+        </h1>
+      </Link>
+      <div>
+        {renderNavLinks()}
+      </div>
+    </nav>
+  )
+}
 
 export default Header
